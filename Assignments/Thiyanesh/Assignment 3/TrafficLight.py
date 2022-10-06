a40b846bf5231a9e20845501823d150ebf9736a1
sthiyanesh <sthiyanesh1@gmail.com>
2022-10-06 16:36:50 +0530

    Adding Thiyanesh S Assignment 3

--- Assignments/Thiyanesh/Assignment 3/TrafficLight.py
+++ Assignments/Thiyanesh/Assignment 3/TrafficLight.py
@@ -1,18 +1,10 @@
-from gpiozero import Button, TrafficLights, Buzzer    
+from gpiozero import TrafficLights    
 from time import sleep    
-    
-buzzer = Buzzer(15)    
-button = Button(21)    
-lights = TrafficLights(25, 8, 7)    
-    
+lights = TrafficLights(25, 8, 7)     
 while True:    
-           button.wait_for_press()   
-           buzzer.on()   
-           light.green.on()    
-           sleep(1)    
-           lights.amber.on()    
-           sleep(1)    
-           lights.red.on()    
-           sleep(1)    
-           lights.off()   
-           buzzer.off()  
+    lights.green.on()    
+    sleep(5)    
+    lights.amber.on()    
+    sleep(1)    
+    lights.red.on()    
+    sleep(5)      
